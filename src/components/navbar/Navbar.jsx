d9b45e2301745a2f14317d9dca07136caf74889a
ShoutingTable <Christopheremanuel97@gmail.com>
2022-08-03 22:06:58 -0500

    modified:   src/App.css 	new file:   src/assets/ai.png 	new file:   src/assets/atlassian.png 	new file:   src/assets/blog01.png 	new file:   src/assets/blog02.png 	new file:   src/assets/blog03.png 	new file:   src/assets/blog04.png 	new file:   src/assets/blog05.png 	new file:   src/assets/dropbox.png 	new file:   src/assets/google.png 	new file:   src/assets/logo.png 	new file:   src/assets/logo.svg 	new file:   src/assets/people.png 	new file:   src/assets/possibility.png 	new file:   src/assets/shopify.png 	new file:   src/assets/slack.png 	modified:   src/components/navbar/Navbar.jsx 	modified:   src/components/navbar/navbar.css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
+const Menu = () => (
+  <>
+  <p><a href = "#home">Home</a></p>
+  <p><a href = "#wcme0502">What is GPT3?</a></p>
+  <p><a href = "#possibility">Open AI</a></p>
+  <p><a href = "#features">Case Studies</a></p>
+  <p><a href = "#blog">Library</a></p>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <div className = "cme0502__navbar">
+      <div className = "cme0502__navbar-links">
+        <div className = "cme0502__navbar-links_logo">
+          <img src = {logo} alt = "logo" />
+        </div>
+        <div className = "cme0502__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className = "cme0502__navbar-sign">
+        <p>Sign in</p>
+        <button type = "button">Sign up</button>
+      </div>
+      <div className = "cme0502__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color = "fff" size = {27} onClick = {() => setToggleMenu(false)} />
+          : <RiMenu3Line color = "fff" size = {27} onClick = {() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className = "cme0502__navbar-menu_container scale-up-center">
+            <div className = "cme0502__navbar-menu_-container-links">
+              <Menu />
+              <div className = "cme0502__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type = "button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
